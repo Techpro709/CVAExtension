464bb599905aa2729957e976eec24067b37b9f04
TechPro709 <17007@mycva.org>
2023-05-28 13:15:10 -0700

    add css to options

--- options.js
+++ options.js
@@ -1,6 +1,13 @@
 const linkInput = document.getElementById("link-input");
 const saveButton = document.getElementById("save-button");
 
+chrome.storage.sync.get(["link"], (result) => {
+  const link = result.link;
+  if (link) {
+    linkInput.value = link;
+  }
+});
+
 saveButton.addEventListener("click", () => {
   const link = linkInput.value;
   chrome.storage.sync.set({ link });
